afed8ceffd0d68f5ac6e5c4bd5a143ef8eb68685
Remi Ete <remi.ete@gmail.com>
2019-11-14 20:58:24 +0100

    Fixed missing REX namespace

--- source/main/LCGeomViewer.cc
+++ source/main/LCGeomViewer.cc
@@ -2,26 +2,31 @@
 #include <TApplication.h>
 namespace REX = ROOT::Experimental ;
 
+// -- tclap headers
+#include <tclap/CmdLine.h>
+#include <tclap/ValueArg.h>
+
+#include <DD4hep/Detector.h>
 
 int main (int argc, const char **argv) {
-  
+
   TCLAP::CmdLine cmd("Linear Collider Geometry Viewer", ' ', "master") ;
 
   TCLAP::ValueArg<std::string> compactFileArg( "g", "geometry",
     "The DD4hep geometry compact file", true, "", "string") ;
   cmd.add( compactFileArg ) ;
-  
+
   cmd.parse( argc, argv ) ;
-  
+
   // Load DD4hep compact file
   auto &detector = dd4hep::Detector::getInstance() ;
   detector.fromCompact( compactFileArg.getValue() ) ;
   auto &manager = detector.manager() ;
-  
+
   TApplication app( "LCGeometryViewer", nullptr, nullptr ) ;
-  auto viewer = std::make_shared<REveGeomViewer>( &manager ) ;
+  auto viewer = std::make_shared<REX::REveGeomViewer>( &manager ) ;
   viewer->Show() ;
   app.Run() ;
-  
+
   return 0 ;
 }
