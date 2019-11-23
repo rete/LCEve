9be8efcd2d6d0b827f2f9ae4f74150b35af8c39e
Remi Ete <remi.ete@gmail.com>
2019-11-23 23:18:45 +0100

    Added ROOTTypes.h with ROOT forward declaration. Also deals with Experimental namespace

---
+++ source/include/LCEve/ROOTTypes.h
@@ -0,0 +1,26 @@
+#pragma once
+
+/// Nice header to deal with ROOT class definitions.
+/// We might remove the Experimental namespace one day...
+
+namespace ROOT {
+  namespace Experimental {
+    class REveManager ;
+    class REveElement ;
+    class REveTrackPropagator ;
+    class REveMagField ;
+    template <class TT> class REveVectorT ;
+    class REveTrack ;
+    class REveScene ;
+    class REveGeomViewer ;
+    class REveGeoShape ;
+    class REveJetCone ;
+    template <typename TT>
+    class REveRecTrackT ;
+    class REvePathMark ;
+    class REvePointSet ;
+  }
+  // Import Experimental namespace in ROOT namespace,
+  // not in global namespace
+  using namespace Experimental ;
+}
