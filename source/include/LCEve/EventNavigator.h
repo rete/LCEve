13b486684ad3282fa16783faa2a6c89fc979ce13
Remi Ete <remi.ete@gmail.com>
2019-11-23 20:48:09 +0100

    Missing file in previous commit

--- source/include/LCEve/EventNavigator.h
+++ source/include/LCEve/EventNavigator.h
@@ -9,10 +9,13 @@ namespace REX = ROOT::Experimental ;
 // -- lcio headers
 #include <MT/LCReader.h>
 #include <EVENT/LCEvent.h>
+#include <EVENT/LCRunHeader.h>
 
 // -- std headers
 #include <memory>
 #include <unordered_map>
+
+#include <LCEve/json.h>
 
 namespace lceve {
 
@@ -26,6 +29,7 @@ namespace lceve {
   class EventNavigator : public REX::REveElement {
   public:
     using RunEventMap = std::vector<std::pair<int, int>> ;
+    using RunHeaderMap = std::map<int, std::unique_ptr<EVENT::LCRunHeader>> ;
 
   public:
     EventNavigator() = delete ;
@@ -46,12 +50,22 @@ namespace lceve {
     /// [Slot] Go to next event
     void NextEvent() ;
 
+    /// Set whether to allow for the event navigation on the user interface
+    void SetAllowUserNavigation( bool allow ) ;
+    /// whether the event navigation on the user interface is allowed
+    bool AllowUserNavigation() const ;
+
+  private:
+    int WriteCoreJson(nlohmann::json &j, int rnr_offset) override ;
+
   private:
     EventDisplay                      *_eventDisplay {nullptr} ;
-    MT::LCReader                      *_lcReader {nullptr} ;
+    std::unique_ptr<MT::LCReader>      _lcReader {nullptr} ;
     std::unique_ptr<EVENT::LCEvent>    _currentEvent {nullptr} ;
     RunEventMap                        _runEventIds {} ;
+    RunHeaderMap                       _runHeaderMap {} ;
     int                                _currentRunEvent {-1} ;
+    bool                               _allowUserNavigation {true} ;
 
     ClassDef( EventNavigator, 0 ) ;
   };
