1ea2f30677074b3f398f0d3c3e1ab087573705a8
Remi Ete <remi.ete@gmail.com>
2019-11-27 00:19:48 +0100

    Adopted ROOT naming conventions

--- source/include/LCEve/Settings.h
+++ source/include/LCEve/Settings.h
@@ -14,27 +14,27 @@ namespace lceve {
   class Settings {
   public:
     /// LCIO read collection names
-    inline void SetReadCollectionNames( const std::vector<std::string> &cols ) { _readCollectionNames = cols ; }
-    inline const std::vector<std::string> &GetReadCollectionNames() const      { return _readCollectionNames ; }
+    inline void SetReadCollectionNames( const std::vector<std::string> &cols ) { fReadCollectionNames = cols ; }
+    inline const std::vector<std::string> &GetReadCollectionNames() const      { return fReadCollectionNames ; }
 
     /// The detector depth level to draw
-    inline void SetDetectorLevel( int level ) { _detectorLevel = level ; }
-    inline int GetDetectorLevel() const       { return _detectorLevel ; }
+    inline void SetDetectorLevel( int level ) { fDetectorLevel = level ; }
+    inline int GetDetectorLevel() const       { return fDetectorLevel ; }
 
     /// Server mode. Whether to show a window on startup or not (server mode = no window at startup)
-    inline void SetServerMode( bool server ) { _serverMode = server ; }
-    inline bool GetServerMode() const        { return _serverMode ; }
+    inline void SetServerMode( bool server ) { fServerMode = server ; }
+    inline bool GetServerMode() const        { return fServerMode ; }
 
     /// DST mode. Load only reconstructed quantities from LCIO files.
     /// Adjust the display according to this mode
-    inline void SetDSTMode( bool dst ) { _dstMode = dst ; }
-    inline bool GetDSTMode() const { return _dstMode ; }
+    inline void SetDSTMode( bool dst ) { fDstMode = dst ; }
+    inline bool GetDSTMode() const { return fDstMode ; }
 
   private:
-    std::vector<std::string>           _readCollectionNames {} ;
-    int                                _detectorLevel {1} ;
-    bool                               _serverMode {false} ;
-    bool                               _dstMode {false} ;
+    std::vector<std::string>           fReadCollectionNames {} ;
+    int                                fDetectorLevel {1} ;
+    bool                               fServerMode {false} ;
+    bool                               fDstMode {false} ;
   };
 
 }
