f7788e7e36c0a45a2efe4553b1fccc640150249e
Remi Ete <remi.ete@gmail.com>
2019-12-07 18:12:37 +0100

    Fixed magnetic field values. Must be -b, not b

--- source/src/BField.cc
+++ source/src/BField.cc
@@ -27,8 +27,8 @@ namespace lceve {
   ROOT::REveVector BField::GetField(Float_t x, Float_t y, Float_t z) const {
     double pos[] = {static_cast<double>(x), static_cast<double>(y), static_cast<double>(z)} ;
     double bfield[3] = {0} ;
-    fEventDisplay->GetGeometry()->GetDetector().field().magneticField(pos, bfield) ;
-    return ROOT::REveVector( bfield[0] / dd4hep::tesla, bfield[1] / dd4hep::tesla, bfield[2] / dd4hep::tesla ) ;
+    fEventDisplay->GetGeometry()->GetDetector().field().combinedMagnetic(pos, bfield) ;
+    return ROOT::REveVector( -bfield[0] / dd4hep::tesla, -bfield[1] / dd4hep::tesla, -bfield[2] / dd4hep::tesla ) ;
   }
 
   //--------------------------------------------------------------------------
