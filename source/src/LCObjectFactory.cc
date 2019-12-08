890bbc07b04ac9b8819fe3af0d5a9cde1fdec7d9
Remi Ete <remi.ete@gmail.com>
2019-12-08 01:13:14 +0100

    Implemented LCIO track conversion. Fixed unit usage in LCIO TrackState conversion

--- source/src/LCObjectFactory.cc
+++ source/src/LCObjectFactory.cc
@@ -17,7 +17,7 @@ namespace lceve {
   TrackState LCObjectFactory::ConvertTrackState( const EVENT::TrackState *const trackState ) const {
     auto bFieldPtr = fEventDisplay->GetGeometry()->GetBField() ;
     auto p = trackState->getReferencePoint() ;
-    auto bfield = bFieldPtr->GetField( p[0], p[1], p[2] ).Mag() ;
+    auto bfield = -bFieldPtr->GetFieldD( p[0]*0.1, p[1]*0.1, p[2]*0.1 )[2] ;
     HelixClass helix ;
     helix.Initialize_Canonical( 
       trackState->getPhi(), trackState->getD0(), trackState->getZ0(), 
@@ -25,7 +25,7 @@ namespace lceve {
     // Fill parameters
     TrackState parameters {} ;
     parameters.fType = static_cast<TrackStateType>(trackState->getLocation()) ;
-    parameters.fReferencePoint = ROOT::REveVectorT<float>( trackState->getReferencePoint() ) ;
+    parameters.fReferencePoint = ROOT::REveVectorT<float>( p[0]*0.1, p[1]*0.1, p[2]*0.1 ) ;
     parameters.fMomentum = ROOT::REveVectorT<float>( helix.getMomentum() ) ;
     return parameters ;
   }  
@@ -34,7 +34,44 @@ namespace lceve {
   
   TrackParameters LCObjectFactory::ConvertTrack( const EVENT::Track *const track ) const {
     TrackParameters parameters {} ;
-#pragma message "Implement LCIO track conversion"
+    auto color = RandomColor( track ) ;
+    // line attributes
+    LineAttributes lineAttr ;
+    lineAttr.fColor = color ;
+    lineAttr.fWidth = 2 ;
+    lineAttr.fStyle = 1 ;
+    parameters.fLineAttributes = lineAttr ;
+    marker attributes
+    MarkerAttributes markerAttr ;
+    markerAttr.fColor = color ;
+    markerAttr.fSize = 3 ;
+    parameters.fMarkerAttributes = markerAttr ;
+    
+    parameters.fCharge = track->getOmega() / std::fabs(track->getOmega()) ;
+    auto tsTypes = { 
+      EVENT::TrackState::AtIP,
+      EVENT::TrackState::AtFirstHit,
+      EVENT::TrackState::AtLastHit,  
+      EVENT::TrackState::AtCalorimeter
+    } ;
+    for( auto tst : tsTypes ) {
+      auto trackState = track->getTrackState(tst) ;
+      if( nullptr != trackState ) {
+        auto trackStateParameters = ConvertTrackState( trackState ) ;
+        parameters.fTrackStates.push_back( trackStateParameters ) ;
+      }
+      // TODO add track state parameters to track properties
+    }
+    auto ref = track->getTrackState( EVENT::TrackState::AtFirstHit ) ;
+    auto p = ref->getReferencePoint() ;
+    parameters.fReferencePoint = ROOT::REveVectorT<float>( p[0]*0.1, p[1]*0.1, p[2]*0.1 ) ;
+    auto bFieldPtr = fEventDisplay->GetGeometry()->GetBField() ;
+    auto bfield = -bFieldPtr->GetFieldD( 0, 0, 0 )[2] ;
+    HelixClass helix ;
+    helix.Initialize_Canonical( 
+      ref->getPhi(), ref->getD0(), ref->getZ0(), 
+      ref->getOmega(), ref->getTanLambda(), bfield ) ;
+    parameters.fMomentum = ROOT::REveVectorT<float>( helix.getMomentum() ) ;
     return parameters ;
   }
   
@@ -50,13 +87,14 @@ namespace lceve {
       std::array<float, 6> errors {} ;
       auto &matrix = vertex->getCovMatrix() ;
       for( unsigned int i=0; i<matrix.size() ; i++ ) {
-        errors[i] = matrix[i] ;
+        errors[i] = matrix[i]*0.1 ;
       }
       parameters.fErrors = errors ;
     }
-    parameters.fPosition = ROOT::REveVectorT<float>( vertex->getPosition() ) ;
+    auto p = vertex->getPosition() ;
+    parameters.fPosition = ROOT::REveVectorT<float>( p[0]*0.1, p[1]*0.1, p[2]*0.1 ) ;
     LineAttributes attr ;
-    attr.fColor = LCObjectFactory::GuessColor( vertex ) ;
+    attr.fColor = RandomColor( vertex ) ;
     parameters.fLineAttributes = attr ;
     PropertyMap properties ;
     properties["Algorithm"] = vertex->getAlgorithmType() ;
